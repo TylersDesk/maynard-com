4a3d91eb574da951bbc00401dbec78ba6ff0dd1f
Tyler Maynard <tyler@hoodoo.digital>
2024-10-07 22:51:45 -0500

    More updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tyler Maynard | Helping men develop the tools to maneuver and establish presence in their life.';
+export const SITE_DESCRIPTION = 'Tyler Maynard is a thought leader and mens work practioner. His goal is to help men connect to their deeper physical and emotional experience to live a full life.';
